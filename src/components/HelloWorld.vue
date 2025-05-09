2a7f42b9c8de85e2feba2d89b38a437ec11af84b
Hadi Permadi <hadipermadi428242>
2025-05-09 20:51:05 +0700

    MEMBUAT APLIKASI DAPAT MENAMBAHKAN KEGIATAN

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,13 @@ import { ref } from 'vue'
 
 const activities = ref(['makan', 'mandi', 'olahraga', 'tidur', 'kuliah'])
 const newActivity = ref('')
+
+function addActivity() {
+  if (newActivity.value.trim() !== '') {
+    activities.value.push(newActivity.value.trim())
+    newActivity.value = ''
+  }
+}
 </script>
 
 <template>
@@ -14,7 +21,7 @@ const newActivity = ref('')
     <div>
       <label for="activityInput">Tambah Kegiatan:</label>
       <input id="activityInput" type="text" v-model="newActivity" />
-      <button type="submit">Submit</button>
+      <button type="submit" @click="addActivity">Submit</button>
     </div>
   </div>
 </template>
