4e758deace48f9da16562eb696ef0ca4d1c32982
Hadi Permadi <hadipermadi428242>
2025-05-09 20:48:47 +0700

    Membuat tampilan agar dapat melihat semua kegiatan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,20 @@
 <script setup>
+import { ref } from 'vue'
 
+const activities = ref(['makan', 'mandi', 'olahraga', 'tidur', 'kuliah'])
+const newActivity = ref('')
 </script>
 
 <template>
-
+  <div>
+    <h2>Todo List</h2>
+    <ul>
+      <li v-for="(activity, index) in activities" :key="index">{{ activity }}</li>
+    </ul>
+    <div>
+      <label for="activityInput">Tambah Kegiatan:</label>
+      <input id="activityInput" type="text" v-model="newActivity" />
+      <button type="submit">Submit</button>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
