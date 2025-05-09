42b2a98df3949e0f75027a816b34c669be9e0fc1
Hadi Permadi <hadipermadi428242>
2025-05-09 21:03:30 +0700

    MENAMBAHKAN TOMBOL CEKLIS UNTUK MENANDAI KEGIATAN YANG SUDAH DI KERJAKAN

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 
 const activities = ref(['makan', 'mandi', 'olahraga', 'tidur', 'kuliah'])
 const newActivity = ref('')
+const doneActivities = ref(new Set())
 
 function addActivity() {
   if (newActivity.value.trim() !== '') {
@@ -13,6 +14,25 @@ function addActivity() {
 
 function removeActivity(index) {
   activities.value.splice(index, 1)
+  doneActivities.value.delete(index)
+  // Adjust doneActivities set for shifted indices
+  const newDone = new Set()
+  doneActivities.value.forEach(i => {
+    if (i > index) {
+      newDone.add(i - 1)
+    } else if (i < index) {
+      newDone.add(i)
+    }
+  })
+  doneActivities.value = newDone
+}
+
+function toggleDone(index) {
+  if (doneActivities.value.has(index)) {
+    doneActivities.value.delete(index)
+  } else {
+    doneActivities.value.add(index)
+  }
 }
 </script>
 
@@ -21,7 +41,8 @@ function removeActivity(index) {
     <h2>Todo List</h2>
     <ul>
       <li v-for="(activity, index) in activities" :key="index">
-        {{ activity }}
+        <input type="checkbox" :checked="doneActivities.has(index)" @change="toggleDone(index)" />
+        <span>{{ activity }}</span>
         <button @click="removeActivity(index)">Hapus</button>
       </li>
     </ul>
