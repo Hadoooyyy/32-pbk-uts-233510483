e795880207a17078d76b01004856d62870175360
Hadi Permadi <hadipermadi428242>
2025-05-09 20:54:51 +0700

    MENAMBAHKAN TOMBOL HAPUS UNTUK MENGHAPUS KEGIATAN

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,13 +10,20 @@ function addActivity() {
     newActivity.value = ''
   }
 }
+
+function removeActivity(index) {
+  activities.value.splice(index, 1)
+}
 </script>
 
 <template>
   <div>
     <h2>Todo List</h2>
     <ul>
-      <li v-for="(activity, index) in activities" :key="index">{{ activity }}</li>
+      <li v-for="(activity, index) in activities" :key="index">
+        {{ activity }}
+        <button @click="removeActivity(index)">Hapus</button>
+      </li>
     </ul>
     <div>
       <label for="activityInput">Tambah Kegiatan:</label>
